7934e06747b630c7c5677522d3d425ac604b44f9
ArtFullStack <asatryandeveloper91@gmail.com>
2022-03-06 14:59:20 +0400

    change file directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/MyPosts/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import "./App.css";
 
 const App = () => {
     return (
