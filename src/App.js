779328521b629868761a5b27fb8eaec6e48f405a
hakob86 <Hakobpoghosyan1986@gmail.com>
2022-03-06 15:08:21 +0400

    change file directory

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/MyPosts/Profile";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
